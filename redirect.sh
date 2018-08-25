beeafefaa1809ae78e58d6a6373419ff09fcbb6f
Carlo Cruz-Albrecht <carlo@berkeley.edu>
2018-08-24 23:59:54 -0700

    redirect.sh <a>

--- redirect.sh
+++ redirect.sh
@@ -9,7 +9,6 @@ title: One moment...
 <meta name="keywords" content="automatic redirection">
 </head>
 <body>
-If your browser does not automatically redirect in a few seconds, 
-<a href='"$1"'>click here</a>.
+If your browser does not automatically redirect in a few seconds, [click here]('"$1"').
 </body>
 </html>'
