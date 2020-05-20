9baf747066fcc0ca3b33bdcfa84b7c008e43f6c1
Carlo <carlo@berkeley.edu>
2020-05-20 05:08:38 -0700

    Cleaned up tinyurl process

--- tinyurl.sh
+++ tinyurl.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-
+#$1 = original (long) url
+#$2 = new (short) url
 cd '/Users/Carlo/Documents/website'
-bash redirect.sh $1 > "$2.md"
-git add "$2.md"
+echo '---
+layout: redirect
+permalink: '"$2"'
+redirect_to: '"$1"'
+---' > "redirect/$2.md"
+git add "redirect/$2.md"
 git commit -m "Making shortcut: carloc.me/$2 --> $1"
 git push origin master
